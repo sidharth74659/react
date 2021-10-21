b8419e935cbbf80fcc065c8764769659cee8bccc
sidharth74659 <49372458+sidharth74659@users.noreply.github.com>
2021-10-22 02:25:34 +0530

    added first component

---
+++ src/App.js
@@ -0,0 +1,5 @@
+// export default () => <h1>Hello from Component</h1>
+// or 
+
+const App = () => <h1>Hello from App Component</h1>
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <App />,
   document.getElementById('root')
 );
